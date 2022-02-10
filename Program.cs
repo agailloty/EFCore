4bec9bf0eef7c202c9a29e8d6c54796762f7755e
agailloty <mr.clerisaxel@hotmail.fr>
2022-02-10 19:01:55 +0100

    Db created. Methods to display and add records to the Table

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using EFCore.Models;
+using EFCore.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCore
 {
@@ -6,7 +10,52 @@ namespace EFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var db = new DataContext();
+            GetPosts(db);
+            Console.Write("Do you want to add new blog post ? (Y/N) : ");
+            string ans = Console.ReadLine();
+            if (ans == "Y")
+            {
+                AddPost();
+            }
+        }
+
+        private static void GetPosts(DataContext db)
+        {
+            var posts = db.Post.AsNoTracking();
+            if (!posts.Any())
+            {
+                Console.WriteLine("**No blog posts found **");
+                return;
+            }
+
+            foreach (var p in posts)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+        private static void AddPost()
+        {
+            // Initialise the classes !
+            var db = new DataContext();
+            var newPost = new Blog();
+            // Fill the data 
+            var randId = new Random();
+            newPost.Id = randId.Next();
+            newPost.Title = Input("Enter the article title : ");
+            newPost.Content = Input("Enter the article content : ");
+            newPost.Category = Input("Category : ");
+            newPost.Published = DateTime.Now;
+
+            db.Add(newPost);
+            db.SaveChanges();
+        }
+
+        private static string Input(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            return input;
         }
     }
 }
